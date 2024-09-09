8d3e10494021d657bfa9070ac9ce046883cca589
custom-discoveries <custom.discoveries@outlook.com>
2024-09-09 17:13:06 -0500

    Updated Cheetah.sh script

--- bin/Cheetah.sh
+++ bin/Cheetah.sh
@@ -3,6 +3,10 @@
 # Copyright (c) 2024, Custom Discoveries Inc.
 # All rights reserved.
 # Cheetah.sh: Bash shell script to run Cheetah application
+# ******************************************************************************
+# Determine which system platform we are running on...
+# ******************************************************************************
+
 #******************************************************************************
 # Run this script in the bin Directory each time you open a new terminal to set
 # the execution path
@@ -10,20 +14,22 @@
 # Alternatively, set CHEETAH_PATH in your bashrc file and append it to the PATH
 # variable to make it permanent
 # ******************************************************************************
-if [ -z $CHEETAH_PATH ]; then
-  echo "Setting CHEETAH_PATH..."
-  CHEETAH_PATH=`pwd`
-  export CHEETAH_PATH
-  export PATH=$PATH:$CHEETAH_PATH
-fi
-# ******************************************************************************
-# Determine which system platform we are running on...
-# ******************************************************************************
 SystemPlatform=`uname`
   if [[ $SystemPlatform == "Linux" ]]; then
     echo "Executing Linux Cheetah RAD Tool"
-      $CHEETAH_PATH/Cheetah-Linux.app/Cheetah
+    if [ -z $CHEETAH_PATH ]; then
+      echo "Setting CHEETAH_PATH..."
+      CHEETAH_PATH=`pwd`/Cheetah-Linux.app
+      export CHEETAH_PATH
+      export PATH=$PATH:$CHEETAH_PATH
+    fi
   elif [[ $SystemPlatform == "Darwin" ]]; then
     echo "Executing Mac Cheetah RAD Tool"
-      $CHEETAH_PATH/Cheetah-Mac.app/Cheetah
+    if [ -z $CHEETAH_PATH ]; then
+      echo "Setting CHEETAH_PATH..."
+      CHEETAH_PATH=`pwd`/Cheetah-Mac-Intel.app
+      export CHEETAH_PATH
+      export PATH=$PATH:$CHEETAH_PATH
+    fi
   fi
+Cheetah
