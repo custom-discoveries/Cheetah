d6a918df8558089e01561f746150d036f1371267
Mitch DeFelice <demit819@customdiscoveries.com>
2024-09-26 13:11:10 -0500

    Release all products V1.0.9

---
+++ bin/initCheetah.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#******************************************************************************
+# Copyright (c) 2024, Custom Discoveries Inc.
+# All rights reserved.
+# Cheetah.sh: Bash shell script to run Cheetah application
+# ******************************************************************************
+# Determine which system platform we are running on...by resetting CHEETAH_PATH
+# ******************************************************************************
+export CHEETAH_PATH=""
+#******************************************************************************
+# Run this script in the bin Directory each time you open a new terminal to set
+# the execution path
+# ******************************************************************************
+# Alternatively, append CHEETAH_PATH in your .bashrc file and append it to the
+# PATH variable to make it permanent
+# ******************************************************************************
+SystemPlatform=`uname`
+Machine=`uname -m`
+  if [[ $SystemPlatform == "Linux" ]]; then
+    echo "Executing Linux Cheetah RAD Tool"
+    if [ -z $CHEETAH_PATH ]; then
+      echo "Setting CHEETAH_PATH..."
+      CHEETAH_PATH=`pwd`/Cheetah-Linux.app
+      export CHEETAH_PATH
+      export PATH=$PATH:$CHEETAH_PATH
+    fi
+    Cheetah
+  elif [[ $SystemPlatform == "Darwin" ]]; then
+    echo "Executing Mac Cheetah RAD Tool"
+    if [ -z $CHEETAH_PATH ]; then
+      if [ $Machine == "x86_64" ]; then
+        echo "Setting CHEETAH_PATH for $Machine..."
+        CHEETAH_PATH=`pwd`/Cheetah-Mac-Intel.app
+        export CHEETAH_PATH
+        export PATH=$PATH:$CHEETAH_PATH
+      elif [ $Machine == "arm64" ]; then
+        echo "Setting CHEETAH_PATH for $Machine..."
+        CHEETAH_PATH=`pwd`/Cheetah-Mac-Arm64.app
+        export CHEETAH_PATH
+        export PATH=$PATH:$CHEETAH_PATH
+      fi
+    fi
+    Cheetah
+  fi
